452ba20f095c6a5aeb8f435c514bea1997b2f977
PadowYT2 <me@padow.ru>
2024-06-28 19:04:05 +0300

    final fixing?

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
 module.exports = require('nextra')({
     theme: 'nextra-theme-docs',
     themeConfig: './theme.config.tsx',
-    staticImage: true,
-    output: 'export',
-})();
+})({ images: { unoptimized: true } });
